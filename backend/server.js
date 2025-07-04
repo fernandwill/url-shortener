ee0dec9944c9ea7128dd4921f2e140c01a57d6f2
Fernand William <fernandwilliam23@gmail.com>
2025-07-04 13:43:34 +0700

    Implemented working URL shortener with backend and frontend

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,11 @@ if (!fs.existsSync(DB_FILE)) fs.writeFileSync(DB_FILE, "[]",
     "utf-8"
 );
 
-const loadUrls = () => JSON.parse(fs.readFileSync(DB_FILE, "utf-8"));
+const loadUrls = () => {
+    const raw = fs.readFileSync(DB_FILE, "utf-8").trim();
+    return raw ? JSON.parse(raw) : {};
+}
+
 const saveUrls = (data) => fs.writeFileSync(DB_FILE, JSON.stringify(data, null, 2));
 
 // Generate 6-chars random ID
@@ -21,7 +25,7 @@ const generateID = () => Math.random().toString(36).slice(2, 8);
 // POST /shorten -> return short URL
 app.post("/shorten", (req, res) => {
     const { url } = req.body;
-    if (!url.startWith("http"))
+    if (!url.startsWith("http"))
         return res.status(400).json({error: "Invalid URL."});
     
     const db = loadUrls();
@@ -29,7 +33,7 @@ app.post("/shorten", (req, res) => {
     db[code] = url;
     saveUrls(db);
 
-    res.json({short: `http:localhost:$(PORT)/${code}`});
+    res.json({short: `http://localhost:${PORT}/${code}`});
 });
 
 // GET /:code -> redirect to URL
--- frontend/app.js
+++ frontend/app.js
@@ -1,1 +1,23 @@
 const input = document.getElementById("url-input");
+const button = document.getElementById("shorten-btn");
+const result = document.getElementById("result");
+
+button.addEventListener("click", async() => {
+    const url = input.value.trim();
+    if (!url) 
+        return alert("Please enter a URL.");
+
+    const res = await fetch("http://localhost:3000/shorten", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ url }),
+
+    });
+
+    const data = await res.json();
+    if (data.short) {
+        result.innerHTML = `Short URL: <a href="${data.short}" target="_blank">${data.short}</a>`;
+    } else {
+        result.textContent = "Failed to shorten URL";
+    }
+});
